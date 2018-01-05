b0b15c354be2a1f50fc3636e011827128bb6a746
Ashish Mishra <as06260@gmail.com>
2018-01-05 16:28:59 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+var app=express();
+app.set('view engine','hbs');
+
+
+hbs.registerPartials(__dirname+'/views/partials')
+
+app.use((req,res,next)=>{
+  res.render('maintenance.hbs');
+});
+
+app.use((req,res,next)=>{
+  var now=new Date().toString();
+  var log=`${now}: ${req.method} ${req.url} `;
+  console.log(log);
+  fs.appendFile('sever.log',log+'\n',(err)=>{
+    if(err)
+    console.log('Not able to put data on file!!!');
+  });
+  next();
+});
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getcurrentyear',()=>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamit',(text)=>{
+  return text.toUpperCase();
+})
+
+app.get('/',(req,res)=>{
+  //res.send('<h1>Hello Express!</h1>');
+  /*res.send({
+    name :"Ashsih",
+    likes:['games','biking','mercedez']
+  });*/
+  res.render('home.hbs',{
+    pagetitle:'Home!! page',
+      hh:'AAAshish MMMishra',
+    mess:'Welcome brooo ksa h sb thik naa chl bye abhi!! kudos to yu!!'
+  //  current_year: new Date().getFullYear()
+  })
+});
+app.get('/about',(req,res)=>{
+//  res.send('About route');
+res.render('about.hbs',{
+  pagetitle:'About page',
+  hh:'AAAshish MMMishra'
+//  current_year: new Date().getFullYear()
+});
+});
+app.get('/bad',(req,res)=>{
+  res.send({
+    errorMessage:"hahahhhahahahhahafiuchkuu!!"
+  });
+});
+app.listen(3000,()=>{
+  console.log('Server is up on port 3000');
+});
